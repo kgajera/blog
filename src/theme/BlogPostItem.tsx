765f87f19eae6d095105f0abd633db7ba95aed40
Kishan Gajera <me@kgajera.com>
2022-03-24 00:30:33 -0400

    feat: add twitter share links to bottom of blog post page

---
+++ src/theme/BlogPostItem.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import OriginalBlogPostItem from "@theme-original/BlogPostItem";
+import type { Props } from "@theme/BlogPostItem";
+import TwitterLink from "../components/TwitterLink";
+
+export default function BlogPostItem(props: Props) {
+  const { siteConfig } = useDocusaurusContext();
+
+  if (props.truncated) {
+    return <OriginalBlogPostItem {...props} />;
+  }
+
+  return (
+    <>
+      <OriginalBlogPostItem {...props} />
+      <div
+        className="margin-vert--lg"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TwitterLink
+          href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(
+            `${siteConfig.url}${props.metadata.permalink}`
+          )}&text=${encodeURIComponent(
+            `I just read "${props.metadata.title}" by @kgajera`
+          )}`}
+          title="Share on Twitter"
+        />
+        <TwitterLink
+          href={`https://twitter.com/search?q=${encodeURIComponent(
+            `${siteConfig.url}${props.metadata.permalink}`
+          )}`}
+          title="Discuss on Twitter"
+        />
+      </div>
+    </>
+  );
+}
